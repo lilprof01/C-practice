1f57f88699e4d99caeb43a4332a680b2cbfc22da
Aniyajuwon <adewumime@gmail.com>
2024-06-30 11:38:19 -0700

    wrote a program that takes two integer and multiplies them and returns the result

---
+++ 2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - entry point multiplies two integers
+ * @a: is the first integer
+ * @b: is the second integer
+ * Return: result of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
